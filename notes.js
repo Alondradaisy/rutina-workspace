793b78cef711014d7b897e1c102558bef63904ec
Alondra Warren <daisy.codeimmersives.com>
2021-05-02 14:58:19 -0700

    get clear functionality to work on notes.js

--- notes.js
+++ notes.js
@@ -2,16 +2,19 @@ const input = document.querySelector('input');
 const button = document.querySelector('button');
 const noteList = document.querySelector('.note-list');
 
+
 button.addEventListener('click', function () {
     const userInput = input.value;
     const note = document.createElement('li');
     note.className = 'note';
     note.innerText = userInput;
-
+    
     noteList.appendChild(note);
 })
 
-function clear(noteList){
-    const clearList = document.getElementsByClassName('.note-list');
-    clearList.parentNode.removeChild(clearList);
-}
+const clearButton = document.getElementById('clear-button');
+clearButton.addEventListener('click', function(event) {
+    event.target.style = 'none';
+    noteList.innerText = ''; 
+    console.log('its working')
+})
